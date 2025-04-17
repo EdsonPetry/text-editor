9f5ed78a616de3ebfc39d8aa890ee6e164024592
richard <richiebbaah@gmail.com>
2025-04-17 11:23:02 -0400

    Fixes #1. Generates file to screen and Now movement Up and down work. If cursor goes beyond original len of file new lines are generated

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <cstdlib> // for system()
 
 // returns the lines of a file in the form of a array
 std::vector<std::string> toVect(std::fstream& file){
@@ -29,6 +30,32 @@ std::ofstream createFile(const std::string& filename){
   MyFile << "{textEditor name} just created " << filename << "\n";
   return MyFile;
 }
+void clearScreen() {
+    // For Linux/macOS
+    std::system("clear");
+
+    // For Windows (if needed)
+    // std::system("cls");
+}
+// Used to display
+void DisplayText(std::vector<std::string> lines,uint cursor){
+  clearScreen();
+  for (size_t i = 0; i < lines.size(); ++i) {
+    if (i != cursor){
+    std::cout << "\t" << i << ": " << lines[i] << "\n";
+    }else{
+    std::cout << ">" << "\t"  << i << ": " << lines[i] << "\n";
+    }
+  }
+  if (cursor >= lines.size()){
+    std::cout << ">" << "\t" << cursor << ":  \n";
+  }
+
+
+}
+bool startsWith(const std::string& str, const std::string& prefix) {
+    return str.rfind(prefix, 0) == 0;
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 2){
@@ -54,23 +81,41 @@ int main(int argc, char* argv[]) {
       return -1;
   }
   std::vector<std::string> lines = toVect(EditFile);
-  printArray(lines);
-  return 0;
+  //printArray(lines);
   // Here we grab the file lines and parse it into an array
 
   std::string input;
   std::cout << "Enter something (type 'exit' to quit):\n";
-
+  uint cursor = 0;
   // This loop will keep running until the user types "exit"
   while (true) {
-      std::cout << "> ";
-      std::getline(std::cin, input);  // reads a full line including spaces
+    DisplayText(lines,cursor);
+    std::cout << "> ";
+    std::getline(std::cin, input);  // reads a full line including spaces
 
-      if (input == "exit") {
-          break;
-      }
+    if (input == "exit") {
+        break;
+    }else if (input == "j")
+    {
+      cursor++;
+    }
+    else if (input == "k"){
+      cursor--;
+    }else if(input == "H"){ // Top of file
+      cursor = 0;
+    }else if(input == "M"){ // Middle of file
+      cursor = lines.size() / 2;
+    }else if(input == "L"){ // Bottom of file
+      cursor = lines.size()-1;
+    }
+    else if(startsWith(input,"e ")){
+     std::cout << "Going into edit mode (Gotta implement This)\n" ; 
+    }
+    else{
+    std::cout << "You entered: " << input << "\n";
 
-      std::cout << "You entered: " << input << "\n";
+    }
+
   }
 
   std::cout << "Goodbye!\n";
